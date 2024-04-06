5f29619f79227fd1238207037d381d7128147a14
Shreya Singh <singh.shreya0511@gmail.com>
2024-04-06 20:19:19 +0530

    Implemented the sensitive text

--- client/src/pages/SharedPages/PeerconnectChat.js
+++ client/src/pages/SharedPages/PeerconnectChat.js
@@ -15,186 +15,227 @@ import ChatMsg from "../../components/Discord/ChatMsg";
 import fetchDiscord from "../../components/Discord/getDiscord";
 
 function PeerconnectChat() {
-	const dummy = useRef(null);
-	const { user } = AuthData();
-	const [discordChat, setDiscordChat] = useState(null);
-	const [message, setMessage] = useState("");
-	const [pickerVisible, setPickerVisible] = useState(false);
-	const [usersChats, setUsersChats] = useState([]);
-	useEffect(() => {
-		dummy.current.scrollIntoView();
-	}, [usersChats, message]);
+  const dummy = useRef(null);
+  const { user } = AuthData();
+  const [discordChat, setDiscordChat] = useState(null);
+  const [message, setMessage] = useState("");
+  const [pickerVisible, setPickerVisible] = useState(false);
+  const [usersChats, setUsersChats] = useState([]);
+  useEffect(() => {
+    dummy.current.scrollIntoView();
+  }, [usersChats, message]);
 
-	const getMsgs = async () => {
-		fetch("http://localhost:5001/api/v1/discord/getMessages", {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				authorization: `Bearer ${getCookies("jwt")}`,
-			},
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				return response.json();
-			})
-			.then((data) => {
-				setUsersChats(data);
-			})
-			.catch((error) => {
-				console.error(
-					"There was a problem with the fetch operation:",
-					error
-				);
-			});
-	};
-	useEffect(() => {
-		fetchDiscord().then((data) => {
-			// console.log("data", data);
-			setDiscordChat(data);
-		});
-		getMsgs();
-	}, []);
-	var socket;
-	useEffect(() => {
-		socket = io("http://localhost:5001");
-		socket.emit("setup", {
-			_id: JSON.parse(user.user)._id,
-		});
-		socket.emit("join discord");
-		socket.on("message received", (newmsgRec) =>
-			handleNewMessageReceived(newmsgRec)
-		);
-		return () => {
-			socket.disconnect();
-		};
-	}, []);
-	const handleNewMessageReceived = async (newmsgRec) => {
-		await getUser(newmsgRec.sender)
-			.then((data) => {
-				newmsgRec.sender = data;
-			})
-			.then(() => {
-				getMsgs();
-			});
-	};
+  const getMsgs = async () => {
+    fetch("http://localhost:5001/api/v1/discord/getMessages", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${getCookies("jwt")}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsersChats(data);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  };
+  useEffect(() => {
+    fetchDiscord().then((data) => {
+      // console.log("data", data);
+      setDiscordChat(data);
+    });
+    getMsgs();
+  }, []);
+  var socket;
+  useEffect(() => {
+    socket = io("http://localhost:5001");
+    socket.emit("setup", {
+      _id: JSON.parse(user.user)._id,
+    });
+    socket.emit("join discord");
+    socket.on("message received", (newmsgRec) =>
+      handleNewMessageReceived(newmsgRec)
+    );
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+  const handleNewMessageReceived = async (newmsgRec) => {
+    await getUser(newmsgRec.sender)
+      .then((data) => {
+        newmsgRec.sender = data;
+      })
+      .then(() => {
+        getMsgs();
+      });
+  };
 
-	const handleNewMessage = async (newMessageReceived) => {
-		console.log("Message Received");
-		newMessageReceived.chat = await fetchDiscord();
-		console.log("after everything", newMessageReceived);
-		const socket = io("http://localhost:5001");
-		socket.emit("new message", newMessageReceived);
-		getMsgs();
-	};
-	const submitForm = async () => {
-		await fetch("http://localhost:5001/api/v1/discord/sendMessage", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				authorization: `Bearer ${getCookies("jwt")}`,
-			},
-			body: JSON.stringify({
-				message: message,
-				userId: JSON.parse(user.user)._id,
-			}),
-		})
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok");
-				}
-				return response.json();
-			})
-			.then((data) => {
-				console.log(data);
-				handleNewMessage(data);
-			})
-			.catch((error) => {
-				console.error(
-					"There was a problem with the fetch operation:",
-					error
-				);
-			});
-		setMessage("");
-	};
+  const handleNewMessage = async (newMessageReceived) => {
+    console.log("Message Received");
+    newMessageReceived.chat = await fetchDiscord();
+    console.log("after everything", newMessageReceived);
+    const socket = io("http://localhost:5001");
+    socket.emit("new message", newMessageReceived);
+    getMsgs();
+  };
 
-	function press(event) {
-		if (event.keyCode === 13 && !event.shiftKey) {
-			event.preventDefault();
-			submitForm();
-		}
-	}
-	const addEmoji = (e) => {
-		let sym = e.unified.split("-");
-		let codesArray = [];
-		sym.forEach((el) => codesArray.push("0x" + el));
-		let emoji = String.fromCodePoint(...codesArray);
-		setMessage(message + emoji);
-	};
+  async function query(data) {
+    console.log(data);
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/facebook/bart-large-mnli",
+      {
+        headers: {
+          Authorization: "Bearer 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",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    return result;
+  }
 
-	const chat = (index) => {
-		const chatMsg = usersChats[index];
-		const sender = chatMsg.sender;
-		if (!sender) return null;
-		// console.log("sender", sender, "user", JSON.parse(user.user)._id);
-		if (sender._id === JSON.parse(user.user)._id) {
-			// console.log("sender", sender, "user", JSON.parse(user.user)._id);
-			return <ChatMsg key={index} chatMsg={chatMsg} position="right" />;
-		} else {
-			// console.log("sender", sender, "user", user.user);
-			return <ChatMsg key={index} chatMsg={chatMsg} position="left" />;
-		}
-	};
+  const queryAI = async (message) => {
+    try {
+      const response = await query({
+        inputs: message,
+        parameters: { candidate_labels: ["sensitiveInfo"] },
+      });
+      return response; // Assuming the response format matches your requirement
+    } catch (error) {
+      throw new Error("Error querying AI:", error);
+    }
+  };
 
-	const chats = () => {
-		var html = [];
-		for (let i = 0; i < usersChats.length; i++) {
-			html.push(chat(i));
-		}
-		return html;
-	};
+  const submitForm = async () => {
+    if (message == "") return;
 
-	return (
-		<>
-			{pickerVisible && (
-				<div className="emoji-picker-dialog">
-					<Picker data={data} onEmojiSelect={addEmoji} />
-				</div>
-			)}
-			<div className="bot-container">
-				<div className="bot-message-container">
-					{chats()}
-					<div ref={dummy} />
-				</div>
-			</div>
-			<div className="chat-text-box">
-				<div className="emoji-icon">
-					<FontAwesomeIcon
-						icon={faSmile}
-						className="emoji-icon-icon"
-						onClick={() => setPickerVisible(!pickerVisible)}
-					/>
-				</div>
-				<div className="chat-text-box-input-container">
-					<div className="chat-input-box">
-						<textarea
-							id="chat-input-text"
-							className="chat-input"
-							rows="2"
-							placeholder="Type here"
-							onKeyDown={(e) => press(e)}
-							onChange={(e) => setMessage(e.target.value)}
-							value={message}
-						></textarea>
-					</div>
-				</div>
-				<div className="emoji-icon" onClick={() => submitForm()}>
-					<FontAwesomeIcon icon={faArrowRight} />
-				</div>
-			</div>
-		</>
-	);
+    // Add AI logic
+    try {
+      console.log("hereeeeeeeeeeeeeee");
+      const res = await queryAI(message);
+      // Assuming response contains the result of AI logic, you can decide what to do based on it
+      const shouldSendMessage = res.scores[0]; // Adjust this condition as per your logic
+      if (shouldSendMessage >= 0.7) {
+        alert("Message contains sensitive information!!🤷‍♀️ Please avoid sending confidential information through this platform. This platform is just for formal discussions.");
+        return;
+      }
+    } catch (error) {
+      console.error("Error running AI logic:", error);
+    }
+
+    await fetch("http://localhost:5001/api/v1/discord/sendMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${getCookies("jwt")}`,
+      },
+      body: JSON.stringify({
+        message: message,
+        userId: JSON.parse(user.user)._id,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        handleNewMessage(data);
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+    setMessage("");
+  };
+
+  function press(event) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault();
+      submitForm();
+    }
+  }
+  const addEmoji = (e) => {
+    let sym = e.unified.split("-");
+    let codesArray = [];
+    sym.forEach((el) => codesArray.push("0x" + el));
+    let emoji = String.fromCodePoint(...codesArray);
+    setMessage(message + emoji);
+  };
+
+  const chat = (index) => {
+    const chatMsg = usersChats[index];
+    const sender = chatMsg.sender;
+    if (!sender) return null;
+    // console.log("sender", sender, "user", JSON.parse(user.user)._id);
+    if (sender._id === JSON.parse(user.user)._id) {
+      // console.log("sender", sender, "user", JSON.parse(user.user)._id);
+      return <ChatMsg key={index} chatMsg={chatMsg} position="right" />;
+    } else {
+      // console.log("sender", sender, "user", user.user);
+      return <ChatMsg key={index} chatMsg={chatMsg} position="left" />;
+    }
+  };
+
+  const chats = () => {
+    var html = [];
+    for (let i = 0; i < usersChats.length; i++) {
+      html.push(chat(i));
+    }
+    return html;
+  };
+
+  return (
+    <>
+      {pickerVisible && (
+        <div className="emoji-picker-dialog">
+          <Picker data={data} onEmojiSelect={addEmoji} />
+        </div>
+      )}
+      <div className="bot-container">
+        <div className="bot-message-container">
+          {chats()}
+          <div ref={dummy} />
+        </div>
+      </div>
+      <div className="chat-text-box">
+        <div className="emoji-icon">
+          <FontAwesomeIcon
+            icon={faSmile}
+            className="emoji-icon-icon"
+            onClick={() => setPickerVisible(!pickerVisible)}
+          />
+        </div>
+        <div className="chat-text-box-input-container">
+          <div className="chat-input-box">
+            <textarea
+              id="chat-input-text"
+              className="chat-input"
+              rows="2"
+              placeholder="Type here"
+              onKeyDown={(e) => press(e)}
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+              value={message}
+            ></textarea>
+          </div>
+        </div>
+        <div className="emoji-icon" onClick={() => submitForm()}>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PeerconnectChat;
